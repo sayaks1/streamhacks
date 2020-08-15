104d7887989bf0b24d37cdfcbb0f4cd9f293f9c5
Saya <ssolbeeks@gmail.com>
2020-08-14 22:43:37 -0400

    update find_flight func

--- find_flights.py
+++ find_flights.py
@@ -14,6 +14,16 @@ distances = {}
 
 
 with open('airlines.csv') as csv_file:
+    """
+    Opens flight data: check off ORIGIN_CITY_NAME, DEST_CITY_NAME, DISTANCE
+
+    Returns
+    -------
+    origins: List
+    destinations: List
+    distances: Dictionary
+        Maps Tuple of (Origin, Destination) to number of miles
+    """
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -21,60 +31,71 @@ with open('airlines.csv') as csv_file:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            origin = row[2]
-            dest = row[4]
-            distance = row[-2]
+            origin = row[0]
+            dest = row[1]
+            distance = row[2]
             if dest not in flights[origin]:
                 flights[origin].append(dest)
             if origin not in origins:
                 origins.append(origin)
             if dest not in destinations:
                 destinations.append(dest)
-            distances[(origin, dest)] = distance
+            distances[(origin, dest)] = float(distance)
             line_count += 1
     print(f'Processed {line_count} lines.')
 
 
-print(distances)
-print(origins)
-print(destinations)
-
 
 def possible_destinations(origin):
+    """
+    Returns all direct flights from origin
+    """
     return list(set(flights[origin]))
 
 
 def find_flight(origin, dest):
+    """
+    Finds the minimum distance path between origin and dest.
+
+    Parameters
+    ----------
+    origin: str
+        Starting city, state
+    dest: str
+        Ending city, state
+
+    Returns
+    ------
+    path: List[str]
+        Names of airports traveled through
+    """
     path = [origin]
     start = origin
-    layer_destinations = list(set(flights[start]))
+    layer_destinations = possible_destinations(start)
     if dest in layer_destinations:
         finish = dest
         path.append(finish)
-        searching = False
     else:
-        possible_dists = {}
+        possible_dists = {} # maps total distance for a path to the connecting city
         for possible in layer_destinations:
-            if dest in list(set(flights[possible])):
-#                 print(possible)
-                total_dist = float(distances[(origin, possible)]) + float(distances[(possible, dest)])
+            if dest in possible_destinations(possible):
+                total_dist = distances[(origin, possible)] + distances[(possible, dest)]
                 possible_dists[total_dist] = possible
+            else:
+                return "Bad luck today. Too many connections, too bad for the environment, too bad for you."
         connection = possible_dists[min(possible_dists.keys())]
         path.append(connection)
         path.append(dest)
         
     return path
 
-
+#example: returns all direct flights out of Rochester
 possible_destinations("Rochester, NY")
 
-
+#example: Finds shortest path from Rochester to Las Vegas
 find_flight("Rochester, NY", "Las Vegas, NV")
 
 
-
-# to check true value
-float(distances[("Burlington, VT", "Chicago, IL")]) + float(distances[("Chicago, IL", 'Los Angeles, CA')])
 
 
 
